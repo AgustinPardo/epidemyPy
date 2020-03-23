42dbfcadb90dd45bf054088370641bef597ec126
AgustinPardo <agu_pardo@hotmail.com>
2020-03-23 12:16:35 -0300

    debug and figure

--- sir.py
+++ sir.py
@@ -2,20 +2,21 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# Argentina numbers
-cd=[1,1,2,8,9,12,17,19,21,31,34,45,56,65,78,97,128,158,225,266]
-tt=t[:len(cd)]
 
 # Total population, N.
-N = 1000
+N = 100000
 # Initial number of infected and recovered individuals, I0 and R0.
 I0, R0 = 1, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
+beta, gamma = 0.2, 0.1
 # A grid of time points (in days)
-t = np.linspace(0, 160, 160)
+t = np.linspace(0, 500,500)
+
+# Out data
+cd=[1,1,2,8,9,12,17,19,21,31,34,45,56,65,78,97,128,158,225,266]
+tt=t[:len(cd)]
 
 # The SIR model differential equations.
 def deriv(y, t, N, beta, gamma):
@@ -33,14 +34,14 @@ S, I, R = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
-ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
-ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
+ax = fig.add_subplot(111, axisbelow=True)
+ax.plot(t, S, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, I, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(tt, cd, 'y', alpha=0.5, lw=2, label='data')
-ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.plot(t, R, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.set_xlabel('Time /days')
-ax.set_ylabel('Number (1000s)')
-ax.set_ylim(0,1.2)
+ax.set_ylabel('N')
+#ax.set_ylim(0,1.2)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
